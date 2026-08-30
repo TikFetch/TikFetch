06b8594dccdcbeeca55d440f13f75df78fce5c3f
Berke Akçen <berkeakcen@gmail.com>
2026-08-30 19:47:17 +0300

    Cover fast-start yt-dlp resolution

---
+++ src/test/java/dev/despical/tikfetch/service/download/YtDlpTikTokDownloadServiceTest.java
@@ -0,0 +1,41 @@
+/*
+ * TikFetch - A clean web app for saving TikTok videos and photo posts.
+ * Copyright (C) 2026  Berke Akçen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+package dev.despical.tikfetch.service.download;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Despical
+ * <p>
+ * Created at 30.08.2026
+ */
+class YtDlpTikTokDownloadServiceTest {
+
+    @Test
+    void retriesTemporaryTikTokExtractorFailures() {
+        assertThat(YtDlpTikTokDownloadService.isRetryableTikTokError(
+            "ERROR: [TikTok] 7678345214133685512: Unexpected response from webpage request"
+        )).isTrue();
+        assertThat(YtDlpTikTokDownloadService.isRetryableTikTokError(
+            "ERROR: [TikTok] Unable to extract universal data for rehydration"
+        )).isTrue();
+    }
+
+    @Test
+    void doesNotRetryPermanentAccessFailures() {
+        assertThat(YtDlpTikTokDownloadService.isRetryableTikTokError(
+            "ERROR: [TikTok] Your IP address is blocked from accessing this post"
+        )).isFalse();
+        assertThat(YtDlpTikTokDownloadService.isRetryableTikTokError(null)).isFalse();
+    }
+}
